f6ce4ee3abe80d80372b7da4672cca00d54a9cf4
OleksiiKar <oleksiykar@icloud.com>
2026-02-17 15:55:31 +0200

    Update MainTest.java

--- src/test/java/Lab1/MainTest.java
+++ src/test/java/Lab1/MainTest.java
@@ -3,9 +3,6 @@ package Lab1;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-import java.util.Random;
-
 public class MainTest {
 
     @Test
